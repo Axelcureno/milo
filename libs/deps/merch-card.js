506e1c340457f1d0a9b091706936ce3b24889a5d
Axel Cureno Basurto <axel@adobe.com>
2023-12-01 21:00:12 -0800

    Update merch-card.js

--- libs/deps/merch-card.js
+++ libs/deps/merch-card.js
@@ -1,4 +1,4 @@
-// Sat, 02 Dec 2023 04:57:14 GMT
+// Sat, 02 Dec 2023 05:00:03 GMT
 import{html as o,LitElement as R}from"./lit-all.min.js";import{css as b,unsafeCSS as s}from"./lit-all.min.js";var w="(max-width: 378px)";var h="(min-width: 820px)",k="(min-width: 696px)",l="(min-width: 1230px)",p="(min-width: 1600px)";var $=b`
     :host {
         position: relative;
@@ -297,7 +297,7 @@ import{html as o,LitElement as R}from"./lit-all.min.js";import{css as b,unsafeCS
 /* Small Tablet */
 @media screen and ${k} {
     :root {
-        --consonant-merch-card-special-offer-tablet-width: 276px;
+        --consonant-merch-card-special-offers-tablet-width: 276px;
         --consonant-merch-card-catalog-tablet-width: 302px;
         --consonant-merch-card-plans-tablet-width: 302px;
     }
@@ -306,7 +306,11 @@ import{html as o,LitElement as R}from"./lit-all.min.js";import{css as b,unsafeCS
     .three-merch-cards.${t},
     .four-merch-cards.${t} {
         gap: var(--consonant-merch-spacing-m);
-        grid-template-columns: repeat(2, var(--consonant-merch-card-${t}-tablet--width));
+        grid-template-columns: repeat(2, var(--consonant-merch-card-${t}-tablet-width));
+    }
+
+    .merch-card.${t} {
+        width: var(--consonant-merch-card-${t}-tablet-width);
     }
 }
 
